a4924e224a5366d723b429c6cb551c138506bfdd
Willy Huang <willyhuang18@gmail.com>
2026-02-19 12:58:11 -0800

    fix: resolve ESLint line errors

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 import "dotenv/config";
 
 export const config = {
-    port: process.env.PORT || 3000,
-    mongoUri: process.env.MONGODB_URI,
+  port: process.env.PORT || 3000,
+  mongoUri: process.env.MONGODB_URI,
 };
